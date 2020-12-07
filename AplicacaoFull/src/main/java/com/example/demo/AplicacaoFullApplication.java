0b5ccd47ca73022463d97ea3bfdf0f09d4b42815
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2020-12-07 14:35:01 -0300

    07-12

---
+++ AplicacaoFull/src/main/java/com/example/demo/AplicacaoFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoFullApplication.class, args);
+	}
+
+}
---
+++ AplicacaoFull/src/main/java/com/example/demo/ManutencaoTable.java
@@ -0,0 +1,81 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name="tb_manutencao")
+public class ManutencaoTable {
+	
+	
+	// Colunas
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String nome;
+	
+
+	@Column
+	private String categoria;
+	
+	@Column
+	@JsonFormat(pattern="yyyy-mm-dd")
+	private Date data;
+	//fim das colunas
+	
+	//Inicio Getters and Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	//fim Getters and Setters
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
