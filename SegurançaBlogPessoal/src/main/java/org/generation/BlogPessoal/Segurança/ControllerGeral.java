c6b3679966aeeaa29473c35504ca5f98c5d089da
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2020-12-09 17:12:02 -0300

    segurança

---
+++ SegurançaBlogPessoal/src/main/java/org/generation/BlogPessoal/Segurança/ControllerGeral.java
@@ -0,0 +1,32 @@
+package org.generation.BlogPessoal.Segurança;
+
+import org.generation.BlogPessoal.model.Tema;
+import org.generation.BlogPessoal.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ControllerGeral {
+	
+	@Autowired
+	private TemaRepository repository;
+	
+	//localhost:8080 <- direciona p a index
+	@GetMapping(value= "/")
+	public String login() {
+		return "index";
+	}	
+	@GetMapping(value="/cadastrarTema")
+	public String form(){
+		return "formTema";
+	}	
+	
+	@PostMapping(value="/cadastrarTema")
+	public String form(Tema tema){			
+		repository.save(tema);
+		return "redirect:/cadastrarTema";
+	}
+
+}
---
+++ SegurançaBlogPessoal/src/main/java/org/generation/BlogPessoal/model/Role.java
@@ -0,0 +1,43 @@
+package org.generation.BlogPessoal.model;
+
+import java.util.List;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Role implements GrantedAuthority {
+	
+	@Id
+	private String nomeRole;
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<Usuario> usuarios;
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return this.nomeRole;
+	}
+	//getters and setters
+
+	public String getNomeRole() {
+		return nomeRole;
+	}
+
+	public void setNomeRole(String nomeRole) {
+		this.nomeRole = nomeRole;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	
+
+}
