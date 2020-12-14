7595ea981248a555c765555742ebcc2a200b5deb
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2020-12-14 14:34:17 -0300

    14/12

---
+++ SegurançaBlogPessoal/src/main/java/org/generation/BlogPessoal/controller/Service.java
@@ -0,0 +1,21 @@
+package org.generation.BlogPessoal.controller;
+
+
+import org.generation.BlogPessoal.model.Usuario;
+import org.generation.BlogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario CadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		return repository.save(usuario);
+	}
+}
